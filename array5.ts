8b92c19e07ba78bd66cf8716d2fa319b1f3b75ba
Vyankatesh Rohokale <piyushrohokale2525@gmail.com>
2024-09-15 11:30:42 -0700

    Add files via upload

---
+++ array5.ts
@@ -0,0 +1,15 @@
+
+var marks : number[] = [11,21,51,101,111];
+
+console.log("number of elements in the array are : "+marks.length);
+
+var icnt : number = 0;
+var i : number = 0;
+for(icnt = 0; icnt < marks.length; icnt++)
+{
+
+    console.log(marks[icnt]);
+    
+
+}
+
