c744c3847fcb6b21d6b2bd4b4655ebd9db96b002
Vyankatesh Rohokale <piyushrohokale2525@gmail.com>
2024-09-21 09:13:55 -0700

    Add files via upload

---
+++ functionarray3.ts
@@ -0,0 +1,15 @@
+
+function demo() : number[]
+{
+    return new Array(10,20,30,40);
+}
+
+var ret : number[] = demo();
+
+console.log("elements of array are : ");
+var i : number = 0;
+
+for(i = 0; i < Arr.length; i++)
+{
+    console.log(Arr[i]);
+}
