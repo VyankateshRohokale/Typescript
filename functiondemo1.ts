64991e847a8541b94f1cdb7b9de1ce7525b7a60f
Vyankatesh Rohokale <piyushrohokale2525@gmail.com>
2024-09-22 09:55:05 -0700

    Add files via upload

---
+++ functiondemo1.ts
@@ -0,0 +1,16 @@
+
+function addition(value1 : number, value2 : number) : number
+{
+   var result : number = 0;
+   result = value1 + value2;
+   return result;
+}
+
+
+var no1 : number = 11;
+var no2 : number = 10;
+var ans : number = 0;
+
+ans = addition(no1,no2);
+
+console.log("addition is : "+ans);
