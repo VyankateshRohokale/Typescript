50fac8a2fab6764facc7acc2b7dea499b283dc73
Vyankatesh Rohokale <piyushrohokale2525@gmail.com>
2024-09-23 09:30:30 -0700

    Add files via upload

---
+++ iteration3.ts
@@ -0,0 +1,16 @@
+function display() : void
+{
+    var icnt : number = 0;
+    var ino : number = 5;
+
+    icnt = 1;
+
+    do
+    {
+        console.log("jay ganesh..");
+        icnt++
+    }while(icnt <= ino)
+
+}
+
+display();
