a1d5e2785d8ed3642e06c605598668d3fe48d73f
Vyankatesh Rohokale <piyushrohokale2525@gmail.com>
2024-09-24 06:45:24 -0700

    Add files via upload

---
+++ lambda1.ts
@@ -0,0 +1,15 @@
+
+function addition(no1 : number, no2 : number) : number{
+    var ans : number = 0;
+    and = no1 + no2;
+    return ans;
+
+}
+
+var result : number = 0;
+result = addition(10,11);
+
+var fun = (no1 : number, no2 : number); => no1 + no2;
+
+result = fun(10,11);
+crossOriginIsolated.log("addition is : " +result);
