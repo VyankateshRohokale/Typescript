867b33c261897cb637aea8d0b03eb9c998c1274f
Vyankatesh Rohokale <piyushrohokale2525@gmail.com>
2024-09-25 06:39:48 -0700

    Add files via upload

---
+++ OOP2.ts
@@ -0,0 +1,19 @@
+
+class Demo
+{
+    // characteristics
+    no1 : number;
+    no2 : number;
+
+    // behaviour
+    display()
+    {
+        console.log("Inside Display Function of Demo class");
+    }
+}
+
+var obj = new Demo();
+
+obj.display();
+
+
