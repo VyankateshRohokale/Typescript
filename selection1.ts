3b2c20dd16107d41ea40b6bbc0d4161819ce1b35
Vyankatesh Rohokale <piyushrohokale2525@gmail.com>
2024-09-30 21:51:25 +0530

    Add files via upload

---
+++ selection1.ts
@@ -0,0 +1,18 @@
+function checkeven(no : number) : void
+{
+    if((no % 2) == 0)
+    {
+        console.log("it is even number");
+    }
+    else
+    {
+        console.log("it is odd number");
+    }
+}
+
+var value1 : number = 11;
+var value2 : number = 10;
+
+checkeven(value1);
+
+checkeven(value2);
