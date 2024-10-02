ab5092150fdad11f9b7cd6f7dd60264bb9772c35
Vyankatesh Rohokale <piyushrohokale2525@gmail.com>
2024-10-02 22:41:04 +0530

    Add files via upload

---
+++ tuple1.ts
@@ -0,0 +1,11 @@
+var data = [99, "python", 90, true]
+
+console.log(typeof data);
+
+console.log("number of elements" +data.length);
+
+var i : number = 0;
+for(i = 0; i < data.length; i++)
+{
+    console.log(data[i]);
+}
